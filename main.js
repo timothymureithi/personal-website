f27535d219b6b966e7eccbf6a1c05d3aa2688587
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-05-31 20:00:03 +0300

    feat:add toggle bar feature

---
+++ main.js
@@ -0,0 +1,8 @@
+// menu toggle bar
+function menu(){
+    let menu = document.getElementById("menu");
+    let menubar = document.querySelector(".menu-btn i");
+
+    menu.classList.toggle("active");
+    menubar.classList.toggle("active");
+}
