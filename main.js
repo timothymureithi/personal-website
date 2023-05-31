73b6b0d43f18bef0b7c8e268aef98f75bf833347
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-05-31 20:02:05 +0300

    add sticky navbar function and scroll to top button function

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+// sticky navbar
+window.onscroll = function(){
+
+    var navbar = document.getElementById("navbar");
+
+    if(window.scrollY > 55){
+        navbar.classList.add("sticky");
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+
+    // scroll to top
+    let button = document.getElementById("btn");
+
+    if(document.documentElement.scrollTop > 90){
+        button.style.display = "block";
+    }
+    else{
+        button.style.display = "none";
+    }
+
+};
+
+function scrolltoTop(){
+    document.documentElement.scrollTop = 0;
+}
+
 // menu toggle bar
 function menu(){
     let menu = document.getElementById("menu");
